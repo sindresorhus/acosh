ca20e4c5b8ab0f473c65cc4a8b787782f5fc90c0
Sindre Sorhus <sindresorhus@gmail.com>
2016-01-01 14:57:38 +0100

    update tests for latest AVA version

--- test.js
+++ test.js
@@ -1,17 +1,16 @@
-'use strict';
-var test = require('ava');
-var numberIsNan = require('number-is-nan');
-Math.acosh = undefined;
-var acosh = require('./');
+import test from 'ava';
+import numberIsNan from 'number-is-nan';
 
-test(function (t) {
-	t.assert(numberIsNan(acosh(-1)));
-	t.assert(numberIsNan(acosh(0)));
-	t.assert(numberIsNan(acosh(0.5)));
-	t.assert(numberIsNan(acosh(-1e300)));
-	t.assert(acosh(1) === 0);
-	t.assert(acosh(2) === 1.3169578969248166);
-	t.assert(acosh(1e+160) === 369.10676205960726);
-	t.assert(acosh(Infinity) === Infinity);
-	t.end();
+Math.acosh = undefined;
+const m = require('./');
+
+test(t => {
+	t.true(numberIsNan(m(-1)));
+	t.true(numberIsNan(m(0)));
+	t.true(numberIsNan(m(0.5)));
+	t.true(numberIsNan(m(-1e300)));
+	t.is(m(1), 0);
+	t.is(m(2), 1.3169578969248166);
+	t.is(m(1e+160), 369.10676205960726);
+	t.is(m(Infinity), Infinity);
 });
